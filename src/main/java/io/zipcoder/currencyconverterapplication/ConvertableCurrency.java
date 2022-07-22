e43f81883cc4a78187359295a25ae096aabadffb
Mike <mike@ZipcodersMBP41.fios-router.home>
2022-07-22 13:54:08 -0400

    lab complete

--- src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
@@ -1,7 +1,12 @@
 package io.zipcoder.currencyconverterapplication;
 
 public interface ConvertableCurrency {
+
+    // formula: parameter / rate of desired conversion currency
+    // e.g. australian rate / usd rate tells you how many usd one aud is worth (~.74usd)
+    // have to get the denominator type by overriding inside each class
     default Double convert(CurrencyType currencyType) {
-        return Double.MAX_VALUE;
+        return currencyType.getRate() / this.getCurrencyType().getRate();
     }
+    CurrencyType getCurrencyType();
 }
--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/CanadianDollar.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/CanadianDollar.java
@@ -1,6 +1,12 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
+import sun.util.resources.cldr.sr.CalendarData_sr_Cyrl_RS;
 
 public class CanadianDollar implements ConvertableCurrency {
+    @Override
+    public CurrencyType getCurrencyType() {
+        return CurrencyType.CANADIAN_DOLLAR;
+    }
 }
